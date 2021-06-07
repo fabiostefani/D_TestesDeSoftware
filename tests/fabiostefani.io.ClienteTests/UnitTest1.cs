3dae040d6ca5d98ee789e09080a9adb64267c5ec
Fabio de Stefani <fabiostefani@gmail.com>
2021-06-07 06:44:27 -0300

    Iniciando projeto

---
+++ tests/fabiostefani.io.ClienteTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace fabiostefani.io.ClienteTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
